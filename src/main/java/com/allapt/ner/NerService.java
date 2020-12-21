a7ccbf3fb08a9a4187192b7cceb21827b1038c5c
LuoYeni <934398274@qq.com>
2020-12-21 15:56:55 +0800

    12.21.15

--- src/main/java/com/allapt/ner/NerService.java
+++ src/main/java/com/allapt/ner/NerService.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 public class NerService {
 
     public static List<CoreMap> sentences;
+    public static StanfordCoreNLP pipeline;
 
 
     public static void init() {
@@ -19,12 +20,12 @@ public class NerService {
         // set the list of annotators to run
         props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse");
         // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
-        // props.setProperty("ner.model","src/main/resources/models/apt41-model.ser.gz");
+        // props.setProperty("ner.model","src/main/resources/models/apt2-model.ser.gz");
         props.setProperty("coref.algorithm", "neural");
         // build pipeline
         props.setProperty("ner.useSUTime", "false");
-       props.setProperty("ner.model","models/apt1-model.ser.gz");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+       props.setProperty("ner.model","models/apt3-model.ser.gz");
+         pipeline = new StanfordCoreNLP(props);
         // create a document object
 
 
@@ -35,11 +36,18 @@ public class NerService {
     public void doNer(String s){
         System.out.println(s);
         CoreDocument document = new CoreDocument(s);
+        pipeline.annotate(document);
+       // System.out.println(document.sentences());
+        for(CoreSentence sentence:document.sentences()){
+            List<CoreEntityMention> entityMentions = sentence.entityMentions();
+            System.out.println("Example: entity mentions");
+            for (CoreEntityMention entityMention:entityMentions){
+                System.out.println(entityMention.entityType()+" : "+entityMention);
+            }
+        }
 
-        CoreSentence sentence = document.sentences().get(0);
-        List<CoreEntityMention> entityMentions = sentence.entityMentions();
-        System.out.println("Example: entity mentions");
-        System.out.println(entityMentions);
+
+
         System.out.println();
 
     }
@@ -47,7 +55,7 @@ public class NerService {
 
 
     public static void main(String[] args) {
-       String text= TextUtill.readFileContent("src/main/resources/text/1.txt");
+       String text= TextUtill.readFileContent("src/main/resources/text/2.txt");
         System.out.println(text);
         NerService.init();
        new NerService().doNer(text);
